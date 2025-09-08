cd1bb90599f5253053a52a7d164cf275d5b6a19a
feckom <139545054+feckom@users.noreply.github.com>
2025-09-08 03:35:20 +0200

    Update vmp.bat

--- vmp.bat
+++ vmp.bat
@@ -1,5 +1,5 @@
 python -m venv .venv
 call .venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-pip install pygame numpy mutagen pydub websockets
-python vmp.py --debug --music-dir "music" --backgrounds "backgrounds" 
+pip install pygame numpy mutagen pydub
+python vmp.py --debug --music-dir "music" --backgrounds "backgrounds"
